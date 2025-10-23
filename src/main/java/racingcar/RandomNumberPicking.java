279b0d6e4839177ddcd0d7aafe45789fece818a2
윤준환 <yunjunhwan2@gmail.com>
2025-10-23 19:58:56 +0900

    feat:RandomNumberPicking organize

---
+++ src/main/java/racingcar/RandomNumberPicking.java
@@ -0,0 +1,11 @@
+package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberPicking {
+
+    public static int pickRandomNumber() {
+
+        //0~9까지
+        return Randoms.pickNumberInRange(0, 9);
+    }
+}
